6bf736b3d97ad87e1adc456982d57bf96205b008
sqyyy <62748369+sqyyy-jar@users.noreply.github.com>
2022-11-02 13:29:56 +0100

    2 November 2022

---
+++ Aggregation.java
@@ -0,0 +1,14 @@
+public class Aggregation {
+    private Teil teil;
+    
+    public Aggregation() {
+    }
+
+    public void setTeil(Teil teil) {
+        this.teil = teil;
+    }
+
+    public Teil getTeil() {
+        return teil;
+    }
+}
---
+++ Komposition.java
@@ -0,0 +1,15 @@
+public class Komposition {
+    private Teil teil;
+
+    public Komposition(Teil teil) {
+        this.teil = teil;
+    }
+
+    public void setTeil(Teil teil) {
+        this.teil = teil;
+    }
+
+    public Teil getTeil() {
+        return teil;
+    }
+}
